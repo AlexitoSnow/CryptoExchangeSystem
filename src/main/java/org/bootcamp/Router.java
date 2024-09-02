a3676d00426d06f1b1fe9dd19eda391872db2cf6
AlexitoSnow <ganieves@espol.edu.ec>
2024-09-02 00:33:33 -0500

    refactor(routing): create Market Route
    - Overcharge the navigateTo method
    - Adds the parameters to pushing information to the routes

--- src/main/java/org/bootcamp/Router.java
+++ src/main/java/org/bootcamp/Router.java
@@ -2,17 +2,25 @@ package org.bootcamp;
 
 import org.bootcamp.controllers.HomeController;
 import org.bootcamp.controllers.RootController;
+import org.bootcamp.controllers.MarketController;
+import java.util.Map;
 
 public abstract class Router {
     public static final String HOME = "home";
     public static final String ROOT = "root";
+    public static final String MARKET = "market";
 
-    public static void navigateTo(String route) {
+    public static void navigateTo(String route, Map<String, Object> parameters) {
         switch (route) {
             case HOME: new HomeController().run();
             break;
+            case MARKET: new MarketController((Integer) parameters.get("marketAction")).run();
             case ROOT:
             default: new RootController().run();
         }
+    }
+
+    public static void navigateTo(String route) {
+        navigateTo(route, null);
     }
 }
