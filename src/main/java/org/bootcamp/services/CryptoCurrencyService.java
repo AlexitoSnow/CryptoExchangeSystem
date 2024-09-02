f836ac34e7a2075f4b22a94c45ca3d3d713633cf
AlexitoSnow <ganieves@espol.edu.ec>
2024-09-02 00:07:56 -0500

    feat(CryptoCurrencyService): CryptoCurrency price fluctuation method
    ## CryptoCurrency
    
    - adds original value attribute
    
    ## CryptoCurrencyService
    
    - fluctuation method uses the original value and random values and intervals to fluctuate the price at the market

--- src/main/java/org/bootcamp/services/CryptoCurrencyService.java
+++ src/main/java/org/bootcamp/services/CryptoCurrencyService.java
@@ -1,7 +1,6 @@
 package org.bootcamp.services;
 
-import org.bootcamp.models.CryptoCurrency;
-
+import org.bootcamp.models.*;
 import java.math.BigDecimal;
 import java.util.*;
 
@@ -30,17 +29,32 @@ public class CryptoCurrencyService {
 
     public void fluctuateCryptoCurrencyValues() {
         Random random = new Random();
+        //RootView view = new RootView();
+        String message = "*** THE MARKET HAS CHANGED ***";
+        //view.showInfo('\n' + message);
         cryptoCurrencies.forEach(((cryptoCurrency, quantity) -> {
-            BigDecimal currentValue = cryptoCurrency.getCurrentValue();
-            BigDecimal newValue = BigDecimal.ZERO;
+            BigDecimal originalValue = cryptoCurrency.getOriginalValue();
+            BigDecimal newValue;
             // Increase price
             if (random.nextBoolean()){
-                newValue = currentValue.add(new BigDecimal(random.nextInt(100)));
+                newValue = originalValue.add(BigDecimal.valueOf(random.nextDouble(originalValue.doubleValue() * 0.0012)));
             // Decrease price
             } else {
-                newValue = currentValue.subtract(new BigDecimal(random.nextInt(100)));
+                newValue = originalValue.subtract(BigDecimal.valueOf(random.nextDouble(originalValue.doubleValue() * 0.0004)));
             }
-            cryptoCurrency.updateValue(newValue);
+            cryptoCurrency.updateCurrentValue(newValue);
+            // Fluctuación aumentó
+            BigDecimal updatedValue = newValue.subtract(originalValue).setScale(2, RoundingMode.HALF_UP);
+            if (newValue.compareTo(originalValue) > 0) {
+                //view.showSuccessMessage("%s (+%s)".formatted(cryptoCurrency, updatedValue));
+            // Fluctuación bajó
+            } else if (newValue.compareTo(originalValue) < 0) {
+                //view.showError("%s (-%s)".formatted(cryptoCurrency, originalValue.subtract(newValue).setScale(2, RoundingMode.HALF_UP)));
+            // No fluctuó
+            } else {
+                //view.showInfo("%s (%s)".formatted(cryptoCurrency, updatedValue));
+            }
         }));
+        //view.showInfo("*".repeat(message.length()));
     }
 }
